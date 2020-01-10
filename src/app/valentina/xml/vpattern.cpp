05425a07ccb3146b9dc2df84502dc2e4515766d2
Roman Telezhynskyi <kroluku@gmail.com>
2020-01-10 09:58:16 +0200

    Incorrect conversion to newer curve format.
    --HG--
    branch : release

--- src/app/valentina/xml/vpattern.cpp
+++ src/app/valentina/xml/vpattern.cpp
@@ -2452,7 +2452,7 @@ void VPattern::ParseToolTrueDarts(VMainGraphicsScene *scene, const QDomElement &
 
 //---------------------------------------------------------------------------------------------------------------------
 // TODO. Delete if minimal supported version is 0.2.7
-void VPattern::ParseOldToolSpline(VMainGraphicsScene *scene, const QDomElement &domElement, const Document &parse)
+void VPattern::ParseOldToolSpline(VMainGraphicsScene *scene, QDomElement &domElement, const Document &parse)
 {
     SCASSERT(scene != nullptr)
     Q_ASSERT_X(not domElement.isNull(), Q_FUNC_INFO, "domElement is null");
@@ -2488,6 +2488,9 @@ void VPattern::ParseOldToolSpline(VMainGraphicsScene *scene, const QDomElement &
         spline->SetColor(color);
 
         VToolSpline::Create(initData, spline);
+
+        // Convert to newer format
+        SetAttribute(domElement, AttrType, VToolSpline::ToolType);
     }
     catch (const VExceptionBadId &e)
     {
@@ -2620,7 +2623,7 @@ void VPattern::ParseToolCubicBezier(VMainGraphicsScene *scene, const QDomElement
 }
 
 //---------------------------------------------------------------------------------------------------------------------
-void VPattern::ParseOldToolSplinePath(VMainGraphicsScene *scene, const QDomElement &domElement, const Document &parse)
+void VPattern::ParseOldToolSplinePath(VMainGraphicsScene *scene, QDomElement &domElement, const Document &parse)
 {
     SCASSERT(scene != nullptr)
     Q_ASSERT_X(not domElement.isNull(), Q_FUNC_INFO, "domElement is null");
@@ -2679,6 +2682,9 @@ void VPattern::ParseOldToolSplinePath(VMainGraphicsScene *scene, const QDomEleme
         path->SetApproximationScale(approximationScale);
 
         VToolSplinePath::Create(initData, path);
+
+        // Convert to newer format
+        SetAttribute(domElement, AttrType, VToolSplinePath::ToolType);
     }
     catch (const VExceptionBadId &e)
     {
