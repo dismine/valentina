03a8d71e65c82df98bec3ca1203618f845ebca02
Roman Telezhynskyi <dismine@gmail.com>
2020-05-09 13:57:42 +0300

    qOverload was introduced in Qt 5.7.

--- src/app/puzzle/vpiececarrousellayer.cpp
+++ src/app/puzzle/vpiececarrousellayer.cpp
@@ -27,6 +27,7 @@
  *************************************************************************/
 
 #include "vpiececarrousellayer.h"
+#include "../vmisc/backport/qoverload.h"
 
 #include <QVBoxLayout>
 
