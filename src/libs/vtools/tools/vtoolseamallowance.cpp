dismine/valentina

commit 1539abaf4d905653cb746cd9919640990c3196ab

Fix hiding inserted node in a pattern piece after its removal.

src/libs/vtools/tools/vtoolseamallowance.cpp
@@ -1788,7 +1788,6 @@ void VToolSeamAllowance::InitNode(const VPieceNode &node, VMainGraphicsScene *sc
                 tool->setParentItem(parent);
                 tool->SetParentType(ParentType::Item);
                 tool->SetExluded(node.IsExcluded());
-                doc->IncrementReferens(node.GetId());
             }
             tool->setVisible(not node.IsExcluded());//Hide excluded point
             break;
@@ -1797,7 +1796,7 @@ void VToolSeamAllowance::InitNode(const VPieceNode &node, VMainGraphicsScene *sc
         case (Tool::NodeElArc):
         case (Tool::NodeSpline):
         case (Tool::NodeSplinePath):
-            doc->IncrementReferens(data->GetGObject(node.GetId())->getIdTool());
+            // Do nothing
             break;
         default:
             qDebug()<<"Get wrong tool type. Ignore.";

commit 27fe4b15fd61755c51b46a158f38d1c9836ab3cc

Fix regression.
Regression after commit 1539abaf4d905653cb746cd9919640990c3196ab.

src/libs/vtools/tools/vtoolseamallowance.cpp
@@ -1744,7 +1744,9 @@ void VToolSeamAllowance::InitNodes(const VPiece &detail, VMainGraphicsScene *sce
 {
     for (int i = 0; i< detail.GetPath().CountNodes(); ++i)
     {
-        InitNode(detail.GetPath().at(i), scene, this);
+        const VPieceNode &node = detail.GetPath().at(i);
+        InitNode(node, scene, this);
+        doc->IncrementReferens(VAbstractTool::data.GetGObject(node.GetId())->getIdTool());
     }
 }
 