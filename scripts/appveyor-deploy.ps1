a4ea99078d4b4e05ca9d4fa45b09662e9d6035a0
Roman Telezhynskyi <dismine@gmail.com>
2021-05-03 15:04:58 +0300

    Try with PowerShell script.

---
+++ scripts/appveyor-deploy.ps1
@@ -0,0 +1,17 @@
+$env:ROOT_FOLDER = "C:\projects\valentina";
+$env:BUILD_FOLDER = "C:\projects\valentina\build";
+
+if($env:DEPLOY -eq "true") {
+    Write-Host "[CI] Starting packing." -ForegroundColor Green;
+    $env:PYTHON\python.exe $env:ROOT_FOLDER\scripts\deploy.py pack .\package\valentina\ "$env:BUILD_FOLDER\package\valentina-win-$(PLATFORM)-$(QT_VERSION)-$(APPVEYOR_REPO_BRANCH)-$(APPVEYOR_REPO_COMMIT).zip";
+    Write-Host "[CI] Done." -ForegroundColor Green;
+
+    Write-Host "[CI] Uploading." -ForegroundColor Green;
+    $env:PYTHON\python.exe $env:ROOT_FOLDER\scripts\deploy.py upload $env:ACCESS_TOKEN "$env:BUILD_FOLDER\package\valentina-win-$(PLATFORM)-$(QT_VERSION)-$(APPVEYOR_REPO_BRANCH)-$(APPVEYOR_REPO_COMMIT).zip" "/0.7.x/Windows/valentina-win-$(PLATFORM)-$(QT_VERSION)-$(APPVEYOR_REPO_BRANCH)-$(APPVEYOR_REPO_COMMIT).zip";
+    Write-Host "[CI] Uploading has been finished." -ForegroundColor Green;
+    Exit 0;
+}
+else {
+    Write-Host "[CI] No deployment needed." -ForegroundColor Green;
+    Exit 0;
+}
