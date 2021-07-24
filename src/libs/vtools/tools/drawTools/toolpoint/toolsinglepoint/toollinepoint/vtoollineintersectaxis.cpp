25047b329ddc8a6a5ab44d682857508598822a71
Roman Telezhynskyi <dismine@gmail.com>
2021-07-24 12:05:04 +0300

    Fix hover for tool point of intersection line and axis.

--- src/libs/vtools/tools/drawTools/toolpoint/toolsinglepoint/toollinepoint/vtoollineintersectaxis.cpp
+++ src/libs/vtools/tools/drawTools/toolpoint/toolsinglepoint/toollinepoint/vtoollineintersectaxis.cpp
@@ -359,3 +359,10 @@ QString VToolLineIntersectAxis::MakeToolTip() const
             .arg(tr("Label"), current->name());
     return toolTip;
 }
+
+//---------------------------------------------------------------------------------------------------------------------
+void VToolLineIntersectAxis::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
+{
+    VToolLinePoint::hoverEnterEvent(event);
+    mainLine->SetBoldLine(false);
+}
