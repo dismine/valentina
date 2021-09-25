d1e6eb269d083fe36379159cda59d475471e4bf1
Roman Telezhynskyi <dismine@gmail.com>
2021-09-25 19:50:48 +0300

    Separate script starts from CIRRUS_WORKING_DIR.

--- scripts/cirrus-deploy.sh
+++ scripts/cirrus-deploy.sh
@@ -28,7 +28,7 @@ if [[ "$DEPLOY" == "true" ]]; then
     check_failure "Unable to create an archive.";
 
     print_info "Start uploading.";
-    python3 $CIRRUS_WORKING_DIR/scripts/deploy.py upload $ACCESS_TOKEN $CIRRUS_WORKING_DIR/build/valentina-${PLATFORM}-${CIRRUS_CHANGE_IN_REPO}.tar.xz "/0.7.x/Mac OS X/valentina-${PLATFORM}-${CIRRUS_CHANGE_IN_REPO}.tar.xz";
+    python3 $CIRRUS_WORKING_DIR/scripts/deploy.py upload $ACCESS_TOKEN $CIRRUS_WORKING_DIR/valentina-${PLATFORM}-${CIRRUS_CHANGE_IN_REPO}.tar.xz "/0.7.x/Mac OS X/valentina-${PLATFORM}-${CIRRUS_CHANGE_IN_REPO}.tar.xz";
     check_failure "Unable to upload.";
 
     print_info "Successfully uploaded.";
