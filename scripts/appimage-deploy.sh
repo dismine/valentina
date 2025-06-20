1d0cd8ab003a702e0db05f477c361055875a6299
Roman Telezhynskyi <dismine@gmail.com>
2025-06-20 16:49:19 +0300

    Rename deploy folders.

--- scripts/appimage-deploy.sh
+++ scripts/appimage-deploy.sh
@@ -25,7 +25,7 @@ if [[ "$DEPLOY" == "true" ]]; then
     check_failure "Unable to clean stale artifacts.";
 
     print_info "Start uploading.";
-    python3 $CIRRUS_WORKING_DIR/scripts/deploy.py upload $ACCESS_TOKEN $CIRRUS_WORKING_DIR/Valentina-continuous-x86_64.AppImage "/0.7.x/Linux/valentina-${TARGET_PLATFORM}-${ARCH}-${CIRRUS_BRANCH}-${CIRRUS_CHANGE_IN_REPO}.AppImage";
+    python3 $CIRRUS_WORKING_DIR/scripts/deploy.py upload $ACCESS_TOKEN $CIRRUS_WORKING_DIR/Valentina-continuous-x86_64.AppImage "/1.0.x/Linux/valentina-${TARGET_PLATFORM}-${ARCH}-${CIRRUS_BRANCH}-${CIRRUS_CHANGE_IN_REPO}.AppImage";
     check_failure "Unable to upload Valentina's AppImage.";
 
     print_info "Successfully uploaded.";
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -140,7 +140,7 @@ def run_clean(refresh_token):
             sys.exit("ERROR: Invalid access token; try re-generating an "
                      "access token from the app console on the web.")
 
-        clean_folders = ["/0.7.x/Mac OS X", "/0.7.x/Windows", "/0.7.x/Linux"]
+        clean_folders = ["/1.0.x/MacOS", "/1.0.x/Windows", "/1.0.x/Linux"]
         arhive_types = [r'^valentina-Windows10\+-mingw-x64-Qt.*-develop-[a-f0-9]{40}\.exe$',
                         r'^valentina-Windows7\+-mingw-x86-Qt.*-develop-[a-f0-9]{40}\.exe$',
                         r'^valentina-Windows10\+-msvc-x64-Qt.*-develop-[a-f0-9]{40}\.exe$',
