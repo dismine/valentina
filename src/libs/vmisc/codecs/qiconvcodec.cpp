d53777156bff14841e2ccd28f0c3f1db87bac194
Roman Telezhynskyi <dismine@gmail.com>
2025-09-26 11:24:11 +0300

    Remove unused header.

--- src/libs/vmisc/codecs/qiconvcodec.cpp
+++ src/libs/vmisc/codecs/qiconvcodec.cpp
@@ -1,10 +1,9 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
+#include "../defglobal.h"
 #include "qiconvcodec_p.h"
-#include "vmisc/defglobal.h"
 
-#include <dlfcn.h>
 #include <errno.h>
 #include <locale.h>
 #include <stdio.h>
