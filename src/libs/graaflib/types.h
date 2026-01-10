c320edc4537a51ec00b612caed8fef3dc911cea1
Roman Telezhynskyi <dismine@gmail.com>
2026-01-10 20:02:44 +0200

    Fix GCC warning.

--- src/libs/graaflib/types.h
+++ src/libs/graaflib/types.h
@@ -10,13 +10,14 @@ using vertex_id_t = std::size_t;
 using edge_id_t = std::pair<vertex_id_t, vertex_id_t>;
 
 struct edge_id_hash {
-  [[nodiscard]] std::size_t operator()(const edge_id_t& key) const {
-    const auto h1{std::hash<vertex_id_t>{}(key.first)};
-    const auto h2{std::hash<vertex_id_t>{}(key.second)};
+    [[nodiscard]] auto operator()(const edge_id_t &key) const noexcept -> std::size_t
+    {
+        const auto h1{std::hash<vertex_id_t>{}(key.first)};
+        const auto h2{std::hash<vertex_id_t>{}(key.second)};
 
-    // TODO: use something like boost::hash_combine
-    return h1 ^ h2;
-  }
+        // TODO: use something like boost::hash_combine
+        return h1 ^ h2;
+    }
 };
 
 }  // namespace graaf
