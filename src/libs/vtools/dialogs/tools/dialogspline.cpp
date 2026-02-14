8ff8971808c4b9343ba4b46b09f4606c804b8d1a
Roman Telezhynskyi <dismine@gmail.com>
2026-02-14 09:34:45 +0200

    Fix incorrect validation of alias value.

--- src/libs/vtools/dialogs/tools/dialogspline.cpp
+++ src/libs/vtools/dialogs/tools/dialogspline.cpp
@@ -430,8 +430,9 @@ void DialogSpline::ValidateAlias()
     spline.SetAliasSuffix(ui->lineEditAlias->text());
 
     if (QRegularExpression const rx(NameRegExp());
-        ui->lineEditAlias->text().isEmpty() || !rx.match(spline.GetAlias()).hasMatch()
-        || (originAliasSuffix != ui->lineEditAlias->text() && !data->IsUnique(spline.GetAlias())))
+        !ui->lineEditAlias->text().isEmpty()
+        && (!rx.match(spline.GetAlias()).hasMatch()
+            || (originAliasSuffix != ui->lineEditAlias->text() && !data->IsUnique(spline.GetAlias()))))
     {
         flagAlias = false;
         ChangeColor(ui->labelAlias, errorColor);
